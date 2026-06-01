eaeb53c067c15da035f60c2a736c60abcd664f1e
Cass Harrison <bleuisresting@gmail.com>
2026-05-31 19:37:32 -0500

    Create plant_config_sample.py

---
+++ plant_config_sample.py
@@ -0,0 +1,6 @@
+# Sample plant configuration - copy to plant_config_local.py and edit
+plant_config = {
+    'annual_cooling_load_mwh': 500_000,  # <-- REPLACE WITH YOUR DATA
+    'current_water_use_L_per_kwh': 2.5,   # <-- REPLACE WITH YOUR DATA
+    # ... etc
+}
